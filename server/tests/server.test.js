6755444efec5746ea571476be99a489e1ddd768b
Pei Ku <pei.ku@lmrc7734.rcoffice.ringcentral.com>
2017-06-20 16:02:29 -0700

    chkpt

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -104,6 +104,48 @@ describe('GET /todos/:id', () => {
       .end(done)
 
   })
+})
 
+describe('DELETE /todos/:id', () => {
+  it('should delete a todo doc', (done) => {
+    var hexId = todos[0]._id.toHexString();
 
+    supertest(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect( (res) => {
+        expect(res.body.todo._id).toBe(hexId)
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err)
+        }
+
+        Todo.findById(hexId).then( (doc) => {
+          console.log(`<debug> doc:${doc}`);
+
+          expect(doc).toNotExist()
+
+          done()
+        }).catch((e) => done(e))
+      })
+  })
+
+  it('should return 404 if todo not found', (done) => {
+    var id = (new ObjectID()).toHexString()
+
+    supertest(app)
+      .delete(`/todos/${id}`)
+      .expect(404)
+      .end(done)
+
+  })
+
+  it('should return 404 if objectID is invalid', (done) => {
+    supertest(app)
+      .delete('/todos/123abc')
+      .expect(404)
+      .end(done)
+
+  })
 })
